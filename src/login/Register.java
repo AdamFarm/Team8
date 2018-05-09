90e3b7b4991cf172940274d64c55b2557fe99fb6
Adam Farm <adam.farm457@yahoo.com>
2018-05-09 15:26:22 -0500

    Add files via upload

--- src/login/Register.java
+++ src/login/Register.java
@@ -14,15 +14,16 @@ public class Register {
 	String db_user = "Team8";
 	String db_pass = "team8iscool";
 
-	/*
-	 * String user; String pass; String user_id; String priv_id; String sec_id;
-	 * String created_on; String created_by; String last_updated_on; String
-	 * last_updated_by;
-	 */
 	Connection conn = null;
 	Statement stmt = null;
 
 	public void createUser() {
+		
+		// Random ID generator
+		UUID uuid = UUID.randomUUID();
+		String id = uuid.toString();
+		
+		// User input for username and password
 		Scanner user_input = new Scanner(System.in);
 		System.out.println("Create a Username: ");
 		String user = user_input.next();
@@ -30,36 +31,52 @@ public class Register {
 		Scanner pass_input = new Scanner(System.in);
 		System.out.println("Create a Password: ");
 		String pass = pass_input.next();
-		user_input.close();
-		pass_input.close();
+		
+		Scanner perm_input = new Scanner(System.in);
+		System.out.println("Choose Permission Level: ");
+		String perm = perm_input.next();
+		
+	/*	if (perm.toUpperCase() != "STUDENT" && perm.toUpperCase() != "TEACHER" 
+				&& perm.toUpperCase() != "ADMIN") {
+				System.out.println("Permissions must be either Student, Teacher, or Admin");
+		} else {
+	*/{		try {
+				DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
+				conn = DriverManager.getConnection(db_url, db_user, db_pass);
+				System.out.println("Connected database successfully...");
 
-		UUID uuid = UUID.randomUUID();
-		String id = uuid.toString();
-		try {
-			DriverManager.registerDriver(new oracle.jdbc.OracleDriver());
-			conn = DriverManager.getConnection(db_url, db_user, db_pass);
-			System.out.println("Connected database successfully...");
+				System.out.println("Inserting records into the table...");
+				stmt = conn.createStatement();
 
-			System.out.println("Inserting records into the table...");
-			stmt = conn.createStatement();
+				String sql = String.format("INSERT INTO USER_INFO VALUES (?,?,?,?)");
+				PreparedStatement ps = conn.prepareStatement(sql);
+				ps.setString(1, user);
+				ps.setString(2, pass);
+				ps.setString(3, id);
+				ps.setString(4, perm);
 
-			String sql = String.format("INSERT INTO USER_INFO VALUES (?,?,?)");
-			PreparedStatement ps = conn.prepareStatement(sql);
-			ps.setString(1, user);
-			ps.setString(2, pass);
-			ps.setString(3, id);
-
-			ps.execute();
-			ps.close();
-		} catch (Exception ex) {
-			System.err.println(ex);
-		} finally {
-			try {
-				if (stmt != null)
-					conn.close();
-			} catch (SQLException se) {
+				ps.execute();
+				ps.close();
+			} catch (Exception ex) {
+				System.err.println(ex);
+			} finally {
+				try {
+					if (stmt != null)
+						conn.close();
+				} catch (SQLException se) {
+				}
 			}
 		}
+
+		
+		user_input.close();
+		pass_input.close();
+		perm_input.close();
+
+		
+
+		
+		
 	}
 
 	public static void main(String[] args) {
